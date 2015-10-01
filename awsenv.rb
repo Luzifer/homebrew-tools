b77842878d7d4610e76422222de2410f23dc291b
Knut Ahlers <knut@ahlers.me>
2015-10-01 19:47:44 +0200

    awsenv v0.6.0

--- awsenv.rb
+++ awsenv.rb
@@ -3,60 +3,80 @@ require "language/go"
 class Awsenv < Formula
   desc "is a credential store for people with multiple AWS accounts"
   homepage "https://github.com/Luzifer/awsenv"
-  url "https://github.com/Luzifer/awsenv/archive/v0.5.3.tar.gz"
-  sha256 "c01c449e829ad4028ffd029ac4b3c309b7ce23fe911bd4583ec2a5931cca5e4b"
+  url "https://github.com/Luzifer/awsenv/archive/v0.6.0.tar.gz"
+  sha256 "6bebc7d37575c465de07a25d7da142ceecac50ac9d26326a97b9b3bd5afeb21c"
   head "https://github.com/Luzifer/awsenv.git"
 
   depends_on "go" => :build
 
   go_resource "github.com/Sirupsen/logrus" do
     url "https://github.com/Sirupsen/logrus.git",
-        :revision => "55eb11d21d2a31a3cc93838241d04800f52e823d"
+      :revision => "418b41d23a1bf978c06faea5313ba194650ac088"
   end
 
   go_resource "github.com/aws/aws-sdk-go" do
     url "https://github.com/aws/aws-sdk-go.git",
-        :revision => "1c75632eb9b77bb3d8e6416fbd8f3e98fc1367db"
+      :revision => "661aeb3339ad9bd5fd420752ebf18a651d40413e"
+  end
+
+  go_resource "github.com/bgentry/speakeasy" do
+    url "https://github.com/bgentry/speakeasy.git",
+      :revision => "36e9cfdd690967f4f690c6edcc9ffacd006014a0"
+  end
+
+  go_resource "github.com/cpuguy83/go-md2man" do
+    url "https://github.com/cpuguy83/go-md2man.git",
+      :revision => "71acacd42f85e5e82f70a55327789582a5200a90"
   end
 
   go_resource "github.com/gorilla/context" do
     url "https://github.com/gorilla/context.git",
-        :revision => "215affda49addc4c8ef7e2534915df2c8c35c6cd"
+      :revision => "1c83b3eabd45b6d76072b66b746c20815fb2872d"
   end
 
   go_resource "github.com/gorilla/mux" do
     url "https://github.com/gorilla/mux.git",
-        :revision => "8096f47503459bcc74d1f4c487b7e6e42e5746b5"
+      :revision => "49c024275504f0341e5a9971eb7ba7fa3dc7af40"
   end
 
   go_resource "github.com/inconshreveable/mousetrap" do
     url "https://github.com/inconshreveable/mousetrap.git",
-        :revision => "76626ae9c91c4f2a10f34cad8ce83ea42c93bb75"
+      :revision => "76626ae9c91c4f2a10f34cad8ce83ea42c93bb75"
+  end
+
+  go_resource "github.com/russross/blackfriday" do
+    url "https://github.com/russross/blackfriday.git",
+      :revision => "8cec3a854e68dba10faabbe31c089abf4a3e57a6"
   end
 
   go_resource "github.com/satori/go.uuid" do
     url "https://github.com/satori/go.uuid.git",
-        :revision => "242673bbc820e051ef00033e274d32e08ece9e15"
+      :revision => "08f0718b61e95ddba0ade3346725fe0e4bf28ca6"
+  end
+
+  go_resource "github.com/shurcooL/sanitized_anchor_name" do
+    url "https://github.com/shurcooL/sanitized_anchor_name.git",
+      :revision => "244f5ac324cb97e1987ef901a0081a77bfd8e845"
   end
 
   go_resource "github.com/spf13/cobra" do
     url "https://github.com/spf13/cobra.git",
-        :revision => "385fc87e4343efec233811d3d933509e8975d11a"
+      :revision => "d732ab3a34e6e9e6b5bdac80707c2b6bad852936"
   end
 
   go_resource "github.com/spf13/pflag" do
     url "https://github.com/spf13/pflag.git",
-        :revision => "67cbc198fd11dab704b214c1e629a97af392c085"
+      :revision => "b084184666e02084b8ccb9b704bf0d79c466eb1d"
   end
 
   go_resource "github.com/vaughan0/go-ini" do
     url "https://github.com/vaughan0/go-ini.git",
-        :revision => "a98ad7ee00ec53921f08832bc06ecf7fd600e6a1"
+      :revision => "a98ad7ee00ec53921f08832bc06ecf7fd600e6a1"
   end
 
   go_resource "gopkg.in/yaml.v2" do
     url "https://gopkg.in/yaml.v2.git",
-        :revision => "49c95bdc21843256fb6c4e0d370a05f24a0bf213"
+      :revision => "53feefa2559fb8dfa8d81baad31be332c97d6c77"
   end
 
   def install

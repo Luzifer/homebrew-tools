d1d3defcb8044bb3163a23f2abac4f9a175699f8
Knut Ahlers <knut@ahlers.me>
2018-10-03 14:20:40 +0200

    Install awsenv from source
    Signed-off-by: Knut Ahlers <knut@ahlers.me>

--- awsenv.rb
+++ awsenv.rb
@@ -1,10 +1,21 @@
 class Awsenv < Formula
   desc "is a credential store for people with multiple AWS accounts"
   homepage "https://github.com/Luzifer/awsenv"
-  url "https://gobuilder.me/get/github.com/Luzifer/awsenv/awsenv_v0.11.0_darwin-amd64.zip"
-  sha256 "592360d1521e36e4c581679d2b6b03e82aad4f9951b13691510f54d7f789643d"
+  url "https://github.com/Luzifer/awsenv/archive/v0.12.0/v0.12.0.tar.gz"
+  sha256 "8ab1b39ee6ce40a2af7b0155d039bf525b1e3d61ec4167bb41643eee979a5862"
+  head "https://github.com/Luzifer/awsenv.git"
+
+  depends_on "go" => :build
 
   def install
+    mkdir_p "#{buildpath}/src/github.com/Luzifer"
+    ln_s buildpath, "#{buildpath}/src/github.com/Luzifer/awsenv"
+
+    ENV["GOPATH"] = buildpath
+    ENV.append_path "PATH", "#{ENV["GOPATH"]}/bin"
+
+    system "go", "build", "-ldflags", "-X main.version=#{version}", "-o", "awsenv", "github.com/Luzifer/awsenv"
+
     bin.install "awsenv"
   end
 
